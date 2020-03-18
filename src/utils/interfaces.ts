ca9cce4b91cdafe85b053ced7aca1e7d7ecd9c6a
Serhii Kalachnikov <alikilqwer@gmail.com>
2020-03-18 15:45:02 +0200

    fix

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,25 +1,4 @@
 export interface IAnnotation {
-<<<<<<< HEAD
-  annotation: {
-    folder: { _text: string };
-    filename: { _text: string };
-    path: { _text: string };
-    source: { database: any };
-    size: {
-      width: { _text: number };
-      height: { _text: number };
-      depth: { _text: number };
-    };
-    segmented: { _text: number };
-    object: {
-      name: { _text: string };
-      pose: { _text: string };
-      truncated: { _text: number };
-      difficult: { _text: number };
-      bndbox: any;
-    };
-  };
-=======
 	annotation: {
 		folder: { _text: string };
 		filename: { _text: string };
@@ -54,5 +33,4 @@ export interface IObject {
 	ymin?: number;
 	xmax?: number;
 	ymax?: number;
->>>>>>> 4af1ed38d7a1af7ccc8aca4a839094eebde7fc18
 }
