f73da9f2885820de8217519144c2fa7e4ccfba1c
Serhii Kalachnikov <alikilqwer@gmail.com>
2020-03-23 03:30:28 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { DetectionModelTrainer } from "./Training";
 async function train_detection_model() {
 	let trainer = new DetectionModelTrainer();
 	await trainer.setModelTypeAsYOLOv3(); // __model_type: 'yolov3'
-	await trainer.setDataDirectory("fire-dataset");
-	await trainer.setTrainConfig(["fire"], 8, 100, "pretrained-yolov3.h5");
+	await trainer.setDataDirectory("mask");
+	await trainer.setTrainConfig(["mask"], 8, 100, "pretrained-yolov3.h5");
 	await trainer.trainModel();
 }
 train_detection_model();
