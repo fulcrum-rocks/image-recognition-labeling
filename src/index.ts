42501003f4f638fd8c5f77e3dde0d492e1189469
Serhii Kalachnikov <alikilqwer@gmail.com>
2020-04-12 21:49:43 +0300

    Add .env

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { DetectionModelTrainer } from "./Training";
+require("dotenv").config();
 
 async function train_detection_model() {
 	let trainer = new DetectionModelTrainer();
 	await trainer.setModelTypeAsYOLOv3();
-	await trainer.setDataDirectory("mask_500");
-	await trainer.setTrainConfig(["mask"], 8, 100, "pretrained-yolov3.h5");
+	await trainer.setDataDirectory(process.env.DIRECTORY);
+	await trainer.setTrainConfig(
+		process.env.LABELS.split(","),
+		8,
+		30,
+		"pretrained-yolov3.h5"
+	);
 	await trainer.trainModel();
 }
 train_detection_model();
